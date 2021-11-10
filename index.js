a86efd76f48a9ad101e0d589254d1ae9ab170f1d
Sharif33 <sharif.cse.nu@gmail.com>
2021-11-10 13:23:41 +0600

    first commit & bikes collection added

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+require('dotenv').config()
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w273s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('bikeManagement');
+        const bikeCollection = database.collection('bikes');
+        const MyOrder = database.collection('orders');
+
+
+        // GET bikes
+        app.get('/bikes', async (req, res) => {
+            const cursor = bikeCollection.find({});
+            const bikes = await cursor.toArray();
+            res.send(bikes);
+        });
+
+        // GET Single bike
+        app.get('/bikes/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const bike = await bikeCollection.findOne(query);
+            res.json(bike);
+        })
+
+        // POST bike
+        app.post('/bikes', async (req, res) => {
+            const bike = req.body;
+            console.log('hit the post api', bike);
+            const result = await bikeCollection.insertOne(bike);
+            console.log(result);
+            res.json(result)
+        });
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('volunteer Network Server is Runnning')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
